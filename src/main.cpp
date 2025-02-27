cf7e65abbe945000bfa4a6cf4162f2c1a8865c48
Asciugano <samuele.diaferio@fastwebnet.it>
2025-02-27 11:24:26 +0100

    aggiunta la possibilita di mandare messaggi con curl grazie alle richieste http

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,16 @@ int PORT = 5001;
 
 void handleClient(int clientSocket) {
   char buffer[1024] = {0};
-  read(clientSocket, buffer, 1024);
-  std::cout << "Messaggio ricevuto: " << buffer << std::endl;
+  int bytesRead = read(clientSocket, buffer, sizeof(buffer));
+  if (bytesRead > 0) {
+    std::cout << "Messaggio ricevuto: " << buffer << std::endl;
+
+    std::string response = "HTTP/1.1 200 OK\r\nContent-Type: "
+                           "text/plain\r\n\r\nMessaggio ricevuto\n";
+    send(clientSocket, response.c_str(), response.size(), 0);
+  } else {
+    std::cerr << "Errore nella lettura del messaggio dal client." << std::endl;
+  }
   close(clientSocket);
 }
 
@@ -19,17 +27,24 @@ void startServer() {
   struct sockaddr_in address;
   int addrlen = sizeof(address);
 
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    std::cerr << "Errore nella creazione del socket" << std::endl;
+    return;
+  }
+
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(PORT);
 
   if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    std::cerr << "Errore nel binding della porta" << std::endl;
+    std::cerr << "Errore nel binding della porta " << PORT << std::endl;
+    close(server_fd);
     return;
   }
+
   if (listen(server_fd, 5) < 0) {
-    std::cerr << "Errore nell'ascolto sulla porta" << std::endl;
+    std::cerr << "Errore nell'ascolto sulla porta " << PORT << std::endl;
+    close(server_fd);
     return;
   }
 
@@ -47,11 +62,21 @@ void startServer() {
                 std::move(clientSocket))
         .detach();
   }
+
+  close(server_fd); // Chiusura del socket del server
 }
 
 int main() {
-  std::cout << "inserire la porta:" << std::endl;
+  std::cout << "Inserire la porta: ";
   std::cin >> PORT;
+
+  if (PORT < 1024 || PORT > 65535) {
+    std::cerr
+        << "Numero di porta non valido. Utilizzare un numero tra 1024 e 65535."
+        << std::endl;
+    return 1;
+  }
+
   std::cout << "Nodo P2P avviato sulla porta " << PORT << std::endl;
   std::thread serverThread(startServer);
   serverThread.join();
