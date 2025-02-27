c4463f1ec7e04010c572a248b7827e09f2cd6d10
Asciugano <samuele.diaferio@fastwebnet.it>
2025-02-27 11:47:17 +0100

    aggiunto il mess la possibilita di madare messaggi da curl usando -c || --curl

--- mess.sh
+++ mess.sh
@@ -1,7 +1,13 @@
-if [[ $# -ne 2 ]]; then
+if [[ $# -lt 2 ]]; then
   echo "Devi inserire messaggio e porta"
   echo "./mess.sh 'ciao come stai' 5001"
   exit 1
 fi
 
-echo $1 | nc 127.0.0.1 $2
+if [[ $# -eq 2 ]]; then
+  echo $1 | nc 127.0.0.1 $2
+elif [[ $# -eq 3 ]]; then
+  if [[ $3 == "-c" || $3 == "--curl" ]]; then
+    curl -d "$1" 127.0.0.1:$2
+  fi
+fi
